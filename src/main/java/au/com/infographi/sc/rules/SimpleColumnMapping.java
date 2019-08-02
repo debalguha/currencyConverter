709953a38b9151d9e8efc4f8c850d7ccc609a73c
Debal <debalguha@gmail.com>
2019-08-02 23:47:44 +1000

    Sanitizer finalized

--- src/main/java/au/com/infographi/sc/rules/SimpleColumnMapping.java
+++ src/main/java/au/com/infographi/sc/rules/SimpleColumnMapping.java
@@ -7,12 +7,19 @@ import java.util.regex.Pattern;
 
 public class SimpleColumnMapping {
     static Pattern[]patternsToReplaceWithEmpty = new Pattern[]{Pattern.compile("_?\\["), Pattern.compile("\\]")};
-    static Pattern squareParenthesisEncloser = Pattern.compile("[^_]*_\\[([a-zA-Z\\.\\s+]*)\\][^_]*");
+    static String[] stringPatternsToReplaceWithEmpty = new String[]{"_?\\[", "\\]", "\\.", "\\s+"};
+    static Pattern squareParenthesisEncloser = Pattern.compile("[^_]*(_\\[)([a-zA-Z\\.\\s+]*)(\\])[^_]*");
 
     public static Function<String, String> sanitizer() {
         // Sanitizer composition is not associative!!
         return Arrays.stream(patternsToReplaceWithEmpty).map(pattern -> replaceMatchingPatternWithEmptyString(pattern)).reduce((func1, func2) -> func1.andThen(func2)).get()
                 .compose(sanitizeVariableEnclosedInSquareBrackets());
+    }
+
+    public static Function<String, String> newSanitizer() {
+        // Sanitizer composition is not associative!!
+        //return sanitizeVariableEnclosedInSquareBrackets();
+        return sanitizeVariableEnclosedInSquareBrackets_new();
     }
 
     public static Function<String, String> replaceMatchingPatternWithEmptyString(Pattern pattern) {
@@ -23,11 +30,37 @@ public class SimpleColumnMapping {
         return s -> {
             Matcher matcher = squareParenthesisEncloser.matcher(s);
             while(matcher.find()){
-                s = s.replaceAll(matcher.group(1), replaceDotFunction().compose(replaceSpaceFunction()).apply(matcher.group(1)));
+                String variableName = replaceDotFunction().andThen(replaceSpaceFunction()).apply(matcher.group(2));
+                s = findAndReplaceFirst(matcher.group(1), "").andThen(findAndReplaceFirst(matcher.group(2), variableName))
+                        .andThen(findAndReplaceFirst(matcher.group(3), "")).apply(s);
+                //s = s.replaceAll(matcher.group(1), "").replaceAll(matcher.group(2), );
             }
             return s;
 
         };
+    }
+
+    public static Function<String, String> sanitizeVariableEnclosedInSquareBrackets_new() {
+        return s -> {
+            Matcher matcher = squareParenthesisEncloser.matcher(s);
+            StringBuilder builder = new StringBuilder();
+            while(matcher.find()){
+                String variableName = replaceDotFunction().andThen(replaceSpaceFunction()).apply(matcher.group(2));
+                builder.append(findAndReplaceFirst(matcher.group(1), "").andThen(findAndReplaceFirst(matcher.group(2), variableName))
+                        .andThen(findAndReplaceFirst(matcher.group(3), "")).apply(matcher.group(0)));
+                //s = s.replaceAll(matcher.group(1), "").replaceAll(matcher.group(2), );
+            }
+            return builder.toString().isEmpty() ? s : builder.toString();
+
+        };
+    }
+
+    public static Function<String, String> findAndReplaceFirst(String pattern, String replacement) {
+        return s -> s.replaceFirst(escapeIfRequired(pattern), replacement);
+    }
+
+    private static String escapeIfRequired(String pattern) {
+        return pattern.contains("[")  ? pattern.replaceAll("\\[", "\\\\[") : pattern.contains("]") ? pattern.replaceAll("\\]", "\\\\]") : pattern;
     }
 
     public static Function<String, String> replaceDotFunction() {
@@ -36,4 +69,5 @@ public class SimpleColumnMapping {
     public static Function<String, String> replaceSpaceFunction() {
         return s -> s.replaceAll("\\s+", "");
     }
+
 }
--- src/test/java/au/com/infographi/RegexTest.java
+++ src/test/java/au/com/infographi/RegexTest.java
@@ -5,10 +5,17 @@ import java.util.regex.Pattern;
 
 public class RegexTest {
     public static void main(String atgs[]) throws Exception {
-        Pattern pattern = Pattern.compile("[^_]*_\\[([a-zA-Z\\.]*)\\][^_]*");
-        Matcher matcher = pattern.matcher("${_[Member.FamilyName].concat(',').concat(_[Member.GivenName]).concat(_[Member.OtherGivenName]) * _[Member.BenefitCategoryText][0]}");
+        String sourceStr = "${_[Member.FamilyName].concat(',').concat(_[Member.GivenName]).concat(_[Member.OtherGivenName]) * _[Member.BenefitCategoryText][0]}";
+        Pattern pattern = Pattern.compile("[^_]*(_\\[)([a-zA-Z\\.\\s+]*)(\\])[^_]*");
+        //Pattern pattern = Pattern.compile("[^_]*_\\[([a-zA-Z\\.]*)\\][^_]*");
+        Matcher matcher = pattern.matcher(sourceStr);
+        System.out.println(sourceStr);
+        //Matcher matcher = pattern.matcher("${!empty _[Benefit Category][0]}");
+        StringBuilder builder = new StringBuilder();
         while(matcher.find()){
-            System.out.println(String.format("Match details 0 -> %s, 1-> %s", matcher.group(0), matcher.group(1)));
+            System.out.println(String.format("Match details 0 -> %s, 1-> %s, 2-> %s, 3-> %s", matcher.group(0), matcher.group(1), matcher.group(2), matcher.group(3)));
+            builder.append(matcher.group(0));
         }
+        System.out.println(builder.toString());
     }
 }
